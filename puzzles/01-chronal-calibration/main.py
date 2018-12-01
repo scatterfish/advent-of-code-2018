a264d4c5ce2818a36f0d0c5bcb61a00ab74f1b1c
Tyrov <dakingxvi@gmail.com>
2018-12-01 02:21:23 -0500

    modified personal python template

--- puzzles/01-chronal-calibration/main.py
+++ puzzles/01-chronal-calibration/main.py
@@ -1,5 +1,8 @@
 #!/bin/python
 
+import os
+import sys
+
 def main():
 	
 	frequency_sum = 0
@@ -38,4 +41,11 @@ def main():
 	
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("\nKeyboard interrupt received. Exiting...")
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
--- templates/python/main.py
+++ templates/python/main.py
@@ -1,7 +1,18 @@
 #!/bin/python
 
+import os
+import sys
+
 def main():
 	print("Hello, world!")
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()
+	except KeyboardInterrupt:
+		print("\nKeyboard interrupt received. Exiting...")
+		try:
+			sys.exit(0)
+		except SystemExit:
+			os._exit(0)
+
