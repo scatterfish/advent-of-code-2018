289462dafd2ccbdd0895cc695f4bae6d6e5085f8
Tyrov <dakingxvi@gmail.com>
2018-12-01 17:50:19 -0500

    added Rust solution for day 1

--- puzzles/01-chronal-calibration-RUST/src/main.rs
+++ puzzles/01-chronal-calibration-RUST/src/main.rs
@@ -1,3 +1,48 @@
+
+use std::process;
+use std::collections::HashSet;
+
 fn main() {
-	println!("Hello, world!");
+	
+	let input = include_str!("input.txt");
+	
+	let input_lines: Vec<&str> = input.lines().collect();
+	let mut frequency_deltas: Vec<i32> = Vec::new();
+	
+	for line in input_lines {
+		let delta = match line.parse::<i32>() {
+			Ok(i) => i,
+			Err(e) => {
+				eprintln!("Error while trying to parse input deltas!\n{}", e);
+				process::exit(1)
+			}
+		};
+		frequency_deltas.push(delta);
+	}
+	
+	println!("Calculating... Be patient, this may take a while...");
+	
+	let mut frequency_sum = 0;
+	let mut first_repeat = 0;
+	let mut known_frequencies: HashSet<i32> = HashSet::new();
+	
+	let mut found_repeat = false;
+	let mut first_loop = true;
+	while !found_repeat {
+		for i in &frequency_deltas {
+			frequency_sum += i;
+			if known_frequencies.contains(&frequency_sum) && !found_repeat {
+				first_repeat = frequency_sum;
+				found_repeat = true;
+				break;
+			}
+			known_frequencies.insert(frequency_sum);
+		}
+		if first_loop {
+			println!("Calibration: {}", frequency_sum);
+			first_loop = false;
+		}
+	}
+	println!("First repeat frequency: {}", first_repeat);
+	
 }
