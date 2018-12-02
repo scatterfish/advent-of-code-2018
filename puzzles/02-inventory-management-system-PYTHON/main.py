0b2267b3e92f00020013b6d8bc80dcca18d31ce4
Tyrov <dakingxvi@gmail.com>
2018-12-02 01:59:27 -0500

    removed unnecessary code from Python day 2

--- puzzles/02-inventory-management-system-PYTHON/main.py
+++ puzzles/02-inventory-management-system-PYTHON/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import sys
-
 def main():
 	
 	lines = []
@@ -53,11 +50,4 @@ def check_match(string_a, string_b):
 	return shared
 
 if __name__ == "__main__":
-	try:
-		main()
-	except KeyboardInterrupt:
-		print("\nKeyboard interrupt received. Exiting...")
-		try:
-			sys.exit(0)
-		except SystemExit:
-			os._exit(0)
+	main()
